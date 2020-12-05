1e95d26ce25344f54bde8d7a6981dfe92c5cc1bf
Jensen <352180787@qq.com>
2020-12-05 22:02:48 +0800

    inital commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: HelloWorld
+ * Author:   lenovo
+ * Date:     2020/12/5 21:59
+ * Description: none
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈none〉
+ *
+ * @author lenovo
+ * @create 2020/12/5
+ * @since 1.0.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+
+}
